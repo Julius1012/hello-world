651d8ab50e779658b3e4720485dc6568b5503481
Julius Wiklander <julius@wiklander.org>
2026-09-14 22:24:11 -0500

    Update greeting

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ function App() {
     <main>
       <section aria-labelledby="page-title">
         <p className="eyebrow">A tiny React app</p>
-        <h1 id="page-title">Hello, world!</h1>
+        <h1 id="page-title">Hello there!</h1>
         <p className="message">Your new web app is up and running.</p>
       </section>
     </main>
